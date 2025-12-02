1adc20bda2d1f4778998276cfa714d365b541882
Vipin <kpvipin@gmail.com>
2025-12-01 20:32:55 -0500

    Minor issue fix

--- backends/attendence_based_on_photo-api/app/api/auth/me/route.ts
+++ backends/attendence_based_on_photo-api/app/api/auth/me/route.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/lib/prisma";
 export async function GET(req: Request) {
   const user = JSON.parse(req.headers.get("x-user")!);
 
-  const profile = await prisma.user.findUnique({
+  const profile = await prisma.users.findUnique({
     where: { id: user.userId },
   });
 
